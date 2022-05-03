ac8a5d215600a932769ac493b50341c2382893c9
Daniel Sepulveda <cdsi21_@hotmail.com>
2022-05-02 22:43:12 -0600

    Movimiento del jugador en script de player

--- IceClimbersGame/Assets/Scripts/Controls.cs
+++ IceClimbersGame/Assets/Scripts/Controls.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
+﻿//Script que controla los inputs de movimiento del player
+//Creado por Alexis Alvarado.
+//Fecha: 02/05/2022
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controles: MonoBehaviour
+public class Controls: MonoBehaviour
 {
-    //Script que controla los inputs de movimiento del player
-
-    [SerializeField] LayerMask controles;
+    [SerializeField] LayerMask controls;
 
     //Este vector es el que se debe usar con el player
-    public Vector2 direccion;
+    public Vector2 direction;
 
-    private Vector2 inicial;
+    private Vector2 initial;
 
     private BoxCollider2D[] hitboxs;
 
@@ -21,9 +23,9 @@ public class Controles: MonoBehaviour
     void Start()
     {
         //Inicialización de variables
-        inicial = Vector2.zero;
+        initial = Vector2.zero;
         hitboxs = this.GetComponentsInChildren<BoxCollider2D>();
-        activar();
+        activate();
 
 
         //Se posicionan los botones de acuerdo a las dimensiones de la pantalla
@@ -44,49 +46,49 @@ public class Controles: MonoBehaviour
         {
             foreach (Touch i in Input.touches)
             {
-                inicial = Camera.main.ScreenToWorldPoint(i.position);
+                initial = Camera.main.ScreenToWorldPoint(i.position);
 
-                if (inicial.x < Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)).x)
+                if (initial.x < Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)).x)
                 {
                     if (Input.touchCount < 2)
                     {
                         up = 0;
                     }
-                    if (Physics2D.OverlapPoint(inicial, controles) == hitboxs[1])
+                    if (Physics2D.OverlapPoint(initial, controls) == hitboxs[1])
                     {
                         dir = 1;
                     }
-                    else if (Physics2D.OverlapPoint(inicial, controles) == hitboxs[0])
+                    else if (Physics2D.OverlapPoint(initial, controls) == hitboxs[0])
                     {
                         dir = -1;
                     }
                 }
 
-                if (inicial.x >= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)).x)
+                if (initial.x >= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 0, 0)).x)
                 {
                     if (Input.touchCount < 2)
                     {
                         dir = 0;
                     }
-                    if (Physics2D.OverlapPoint(inicial, controles) == hitboxs[2])
+                    if (Physics2D.OverlapPoint(initial, controls) == hitboxs[2])
                     {
                         up = 1;
                     }
                 }
 
-                direccion = new Vector2(dir, up);
+                direction = new Vector2(dir, up);
             }
         }
         else
         {
             dir = 0;
             up = 0;
-            direccion = Vector2.zero;
+            direction = Vector2.zero;
         }
     }
 
     //Activa los gameobjects de los botones en caso de ser necesario
-    void activar()
+    void activate()
     {
         hitboxs[0].gameObject.SetActive(true);
         hitboxs[1].gameObject.SetActive(true);
--- IceClimbersGame/Assets/Scripts/Player.cs
+++ IceClimbersGame/Assets/Scripts/Player.cs
@@ -1,51 +1,88 @@
+//Script para controlar el player
+//Creado por Alexis Alvarado. Modificado por Daniel Sepulveda
+//Fecha: 02/05/2022
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //Script que controla el player
-
-    private Controles control;
+    private Controls control;
     private Animator anim;
     private SpriteRenderer sprtR;
+    private Rigidbody2D body;
+    [SerializeField] private LayerMask floor;
+    private bool isTouchingFloor;
+    private float speed = 1.6f;
+
+    //a los componentes inicializar arriba y en la funcion start localizarlos, en el control busca dentro de la escena un objeto
+    //de tipo control que es el script
 
     void Start()
     {
-        //Inicialización de las variables
-        control = GameObject.FindObjectOfType<Controles>();
+        //Inicializaciï¿½n de las variables
+        control = GameObject.FindObjectOfType<Controls>();
         sprtR = this.GetComponent<SpriteRenderer>();
         anim = this.GetComponent<Animator>();
+        body = this.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         animations();
+        movement();
+    }
+
+    //Funcion para el movimiento
+    private void movement()
+    {
+        body.velocity = new Vector2(control.direction.x, body.velocity.y) * speed;
+        Vector2 position = new Vector2(transform.position.x, transform.position.y - 0.3f);
+        Vector2 size = new Vector2(0.1f, 0.1f);
+
+        if (Physics2D.OverlapBox(position, size, 0).IsTouchingLayers(floor))
+        {
+            isTouchingFloor = true;
+        }
+        else
+        {
+            isTouchingFloor = false;
+        }
+
+        jump();
+    }
+
+    private void jump()
+    {
+        if (isTouchingFloor && control.direction.y>0)
+        {
+            body.velocity = Vector2.up * speed;
+        }
+        else
+        {
+
+        }
     }
 
     //Controla las animaciones del player
     private void animations()
     {
-        //Controla la orientación del sprite
-        if (control.direccion.x < 0) sprtR.flipX = true;
-        else if (control.direccion.x > 0) sprtR.flipX = false;
-
-        //Modificación de prueba sólo para darle push
-        int var = 0;
+        //Controla la orientaciï¿½n del sprite
+        if (control.direction.x < 0) sprtR.flipX = true;
+        else if (control.direction.x > 0) sprtR.flipX = false;
         
         //Parametros de las animaciones
+        anim.SetFloat("Xvelocity", Mathf.Abs(control.direction.x));
+        anim.SetFloat("Yvelocity", body.velocity.y);
 
-        anim.SetFloat("Xvelocity", Mathf.Abs(control.direccion.x));
-        //anim.SetFloat("Yvelocity", aquiVaLaVelocidadYdelRigidBody2D);
-
-        //Este parametro va a depender de si estás tocando el suelo o no
-        anim.SetBool("Suelo", true);
+        //Este parametro va a depender de si estï¿½s tocando el suelo o no
+        anim.SetBool("Floor", isTouchingFloor);
 
         //Se debe volver true cuando presionas el doble salto (solo se necesita que sea true durante un frame)
         anim.SetBool("DJump", false);
 
-        //Se debe volver true cuando recibes daño (solo se necesita que sea true durante un frame)
+        //Se debe volver true cuando recibes daï¿½o (solo se necesita que sea true durante un frame)
         anim.SetBool("Hit", false);
-
     }
 }
